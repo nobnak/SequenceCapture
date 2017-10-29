5b6ee54ad35ac60be70c8d9cb52fb67515b80d19
Naka Nobuyuki <nobu.nakata@gmail.com>
2017-10-29 23:13:04 +0900

    ファイルが存在していたらIDをインクリメント

--- Format/AbstractTextureSerializer.cs
+++ Format/AbstractTextureSerializer.cs
@@ -6,8 +6,10 @@ using UnityEngine.SceneManagement;
 namespace SequenceCaptureSystem.Format {
 
     public abstract class AbstractTextureSerializer : System.IDisposable {
-        const string FORMAT_FILE = "{0}_{1}_{{0:D5}}.{2}";
+        const string FORMAT_FILE = "{0}_{1}_{{0:D5}}_{{1}}.{2}";
         protected readonly string formatPath;
+
+        protected int uniquenessCounter = 0;
 
         public AbstractTextureSerializer(string folder, string extension) {
             formatPath = Path.Combine(folder, string.Format(FORMAT_FILE, 
@@ -17,7 +19,7 @@ namespace SequenceCaptureSystem.Format {
         }
         public virtual bool Serialize(Texture2D tex) {
             try {
-                var path = string.Format(formatPath, Time.frameCount);
+                var path = GetUniquePath();
                 File.WriteAllBytes(path, ToByte(tex));
                 return true;
             } catch (System.Exception e) {
@@ -25,6 +27,19 @@ namespace SequenceCaptureSystem.Format {
             }
             return false;
         }
+        private string GetPath(int id) {
+            return string.Format(formatPath, Time.frameCount, id);
+        }
+        private string GetUniquePath() {
+            var path = GetPath(0);
+            if (!File.Exists(path)) {
+                uniquenessCounter = 0;
+                return path;
+            }
+            while (File.Exists(path = GetPath(++uniquenessCounter))) ;
+            return path;
+        }
+
         public abstract byte[] ToByte(Texture2D tex);
 
         #region IDisposable implementation
