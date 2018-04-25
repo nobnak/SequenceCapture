d27544956c31602cb42f1f90d6bdebb9c77701ed
Nakata Nobuyuki <nobu.nakata@gmail.com>
2018-04-25 11:08:45 +0900

    キャプチャタイミング変更

--- SequenceCaptureScreen.cs
+++ SequenceCaptureScreen.cs
@@ -1,11 +1,30 @@
-﻿namespace SequenceCaptureSystem {
+using System.Collections;
+using UnityEngine;
+
+namespace SequenceCaptureSystem {
 
     public class SequenceCaptureScreen : BaseSequenceCapture {
+		protected Coroutine coroutineCapture;
 
-        #region Unity
-    	protected virtual void OnPostRender() {
-            CapturePerFrame();
-        }
-        #endregion
-    }
+		#region Unity
+		protected override void OnEnable() {
+			base.OnEnable();
+			coroutineCapture = StartCoroutine(ProcessCapture());
+		}
+		protected override void OnDisable() {
+			base.OnDisable();
+			if (coroutineCapture != null) {
+				StopCoroutine(coroutineCapture);
+				coroutineCapture = null;
+			}
+		}
+		#endregion
+
+		protected IEnumerator ProcessCapture() {
+			while (true) {
+				yield return new WaitForEndOfFrame();
+				CapturePerFrame();
+			}
+		}
+	}
 }
